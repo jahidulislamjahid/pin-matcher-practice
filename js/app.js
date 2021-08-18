e57a7a2f377d300ef79cea375200a8d83b30f245
jahid <info.jahidulislamjahid@gmail.com>
2021-08-18 16:52:55 +0600

    pin matcher added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// generating pin 
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
     const pinString = pin + '';
@@ -11,4 +12,40 @@ function getPin() {
 document.getElementById('pin-generator').addEventListener('click', function () {
     getPin();
     document.getElementById('pin-window').value = getPin();
-})
+});
+
+// inputing pin 
+document.getElementById('keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calc = document.getElementById('keypad-number');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calc.value = '';
+        }
+    }
+    else {
+        const prevNumber = calc.value;
+        const newNumber = prevNumber + number;
+        calc.value = newNumber;
+    }
+
+});
+
+function varifyPin() {
+    const pin = document.getElementById('pin-window').value;
+    const numberPressed = document.getElementById('keypad-number').value;
+    const success = document.getElementById('matched');
+    const error = document.getElementById('not-matched');
+    if (pin == numberPressed) {
+        success.style.display = 'block';
+        error.style.display = 'none';
+    }
+    else {
+        error.style.display = 'block';
+        success.style.display = 'none';
+    }
+}
+
+document.getElementById('submit').addEventListener('click', function () {
+    varifyPin();
+});
